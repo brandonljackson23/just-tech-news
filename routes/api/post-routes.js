6b1c2a6d2c55742e3260bd0327b7895555ca58f8
Brandon Jackson <brandonljackson23@gmail.com>
2020-08-01 12:39:34 -0600

    added comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,30 +1,34 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require('../../config/connection');
 
 // get all users
 router.get('/', (req, res) => {
-    Post.findAll({
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
-        order: [['created_at', 'DESC']], 
-        include: [
-          {
-            model: User,
-            attributes: ['username']
-          }
-        ]
-    })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-    });
+  Post.findAll({
+    order: [['created_at', 'DESC']],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+   })
 });
 
 // get a single user
@@ -42,8 +46,12 @@ router.get('/:id', (req, res) => {
       ],
       include: [
         {
-          model: User,
-          attributes: ['username']
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
         }
       ]
     })
